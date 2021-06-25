3f4016b39a267ec05f00289650b249d88ef62bd3
Johir Raihan <jahirraihanmsd@gmail.com>
2021-06-25 23:29:57 +0600

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $token_validity = 24 * 60;
+
+        $this->guard()->factory()->setTTL($token_validity);
+
+        if (!$token = $this->guard()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized', 401]);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'address' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user]);
+    }
+
+    public function logout()
+    {
+        $this->guard()->logout();
+        return response()->json(['message' => 'User logout successfully']);
+    }
+
+    public function profile()
+    {
+        return response()->json($this->guard()->user());
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'token_validity' => $this->guard()->factory()->getTTL() * 60
+        ]);
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controller',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('profile', [AuthController::class, 'profile']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
